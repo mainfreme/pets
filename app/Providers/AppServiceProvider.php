aa7b4e9a7b5151e90533e086a56057c5a6c41881
Soft Well House <bialonj@gmail.com>
2025-01-17 13:03:54 +0100

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use App\Service\PetService;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(PetService::class, function ($app) {
+            return new PetService();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
